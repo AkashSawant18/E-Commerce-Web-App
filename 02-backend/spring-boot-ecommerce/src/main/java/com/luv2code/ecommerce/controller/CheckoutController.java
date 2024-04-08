ec199e944f79817dd92f60e3ee199cc03720a15f
Akash <akashsawant9960@gmail.com>
2024-04-09 00:46:18 +0530

    integrated payment feature in backend

--- 02-backend/spring-boot-ecommerce/src/main/java/com/luv2code/ecommerce/controller/CheckoutController.java
+++ 02-backend/spring-boot-ecommerce/src/main/java/com/luv2code/ecommerce/controller/CheckoutController.java
@@ -1,13 +1,18 @@
 package com.luv2code.ecommerce.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.luv2code.ecommerce.dto.PaymentInfo;
 import com.luv2code.ecommerce.dto.Purchase;
 import com.luv2code.ecommerce.dto.PurchaseResponse;
 import com.luv2code.ecommerce.service.CheckoutService;
+import com.stripe.exception.StripeException;
+import com.stripe.model.PaymentIntent;
 
 @RestController
 @RequestMapping("/api/checkout")
@@ -23,6 +28,16 @@ public class CheckoutController {
 	public PurchaseResponse placeOrder(@RequestBody Purchase purchase) {
 		PurchaseResponse purchaseResponse = checkoutService.placeOrder(purchase);
 		return purchaseResponse;
+	}
+	
+	@PostMapping("/payment-intent")
+	public ResponseEntity<String> createpaymentIntent(@RequestBody PaymentInfo paymentinfo) throws StripeException{
+		PaymentIntent paymentIntent = checkoutService.createPaymentIntent(paymentinfo);
+		
+		String paymentStr = paymentIntent.toJson();
+		
+		return new ResponseEntity<>(paymentStr, HttpStatus.OK);
+		
 	}
 	
 }
